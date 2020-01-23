8936bbac523a749711756642b6bab4655c836f0f
shashank-reddy-electromob <pshashankreddy67@gmail.com>
2020-01-23 22:09:39 +0530

    code check

---
+++ app/src/main/java/org/electromob/new_project/farmerauthentication2.java
@@ -0,0 +1,13 @@
+package org.electromob.new_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class farmerauthentication2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmerauthentication2);
+    }
+}
